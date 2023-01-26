a90337bdf63d42ce4530467650a1af8a1096a002
PavelKazachenokPvls <pavel.kazachenok@pvls.tech>
2023-01-26 15:55:15 +0300

    db architecture

--- autohouse/models.py
+++ autohouse/models.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
 from django_countries.fields import CountryField
 
@@ -18,15 +19,63 @@ class CommonInfo(models.Model):
         abstract = True
 
 
+class CommonDiscount(CommonInfo):
+    car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)
+    discount = models.DecimalField(
+        default=0, decimal_places=2, max_digits=5,
+        validators=[
+            MinValueValidator(0.01),
+            MaxValueValidator(100.00)
+        ]
+    )
+    title = models.CharField(max_length=255, null=True)
+    description = models.TextField(null=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now() + timedelta(days=7))
+
+    class Meta:
+        abstract = True
+
+
+class CommonCarInfo(CommonInfo):
+    car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)
+    supplier = models.ForeignKey('Supplier', on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10,
+                                validators=[MinValueValidator(limit_value=1.00)])
+
+    class Meta:
+        abstract = True
+
+
+class AutohouseDiscount(CommonDiscount):
+    autohouse = models.ForeignKey('Autohouse', on_delete=models.SET_NULL, null=True)
+
+
+class SupplierDiscount(CommonDiscount):
+    supplier = models.ForeignKey('Supplier', on_delete=models.SET_NULL, null=True)
+
+
+class AutohouseCar(CommonCarInfo):
+    autohouse = models.ForeignKey('Autohouse', on_delete=models.SET_NULL, null=True)
+    count = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.car.brand
+
+
 class Autohouse(CommonInfo):
     title = models.CharField(max_length=255)
     location = CountryField()
     prefer_characteristic = models.JSONField(validators=[CharacteristicJSONValidationSchema(
         limit_value=EnumSchemas.CHARACTERISTIC_SCHEMA.value
     )], null=True)
+    balance = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.title
+
+    def car_list(self):
+        return AutohouseCar.objects.filter(autohouse=self)
 
 
 class Car(CommonInfo):
@@ -46,8 +95,15 @@ class Car(CommonInfo):
         choices=TRANSMISSION_CHOICES, max_length=50
     )
     body_type = models.CharField(max_length=50)
-    price = models.DecimalField(decimal_places=2, max_digits=10,
-                                validators=[MinValueValidator(limit_value=1.00)])
 
     def __str__(self):
         return '{} | {}'.format(self.brand, self.model)
+
+
+class SupplierCar(CommonCarInfo):
+    pass
+
+
+class Supplier(CommonInfo):
+    title = models.CharField(max_length=255)
+    balance = models.DecimalField(default=0, decimal_places=2, max_digits=10)
