9a3f785ec8fb6a0f24db027a294b6978d245a228
PavelKazachenokPvls <pavel.kazachenok@pvls.tech>
2023-02-08 13:06:53 +0300

    DONT FORGET TO RESTORE

--- autohouse/tasks.py
+++ autohouse/tasks.py
@@ -8,7 +8,17 @@ from main.models import Car
 from supplier.models import SupplierCar, SupplierDiscount
 
 
+def get_cheapest_car(cars):
+    cars = list(filter(lambda item: item is not None, cars))
+    if cars:
+        cheapest = sorted(cars, key=lambda item: item.price)[0]
+        return cheapest
+    return False
+
+
 def best_proposition(cars):
+    min_discount_price_list = []
+    min_default_price_list = []
     for car in cars:
         discounts = SupplierDiscount.objects.filter(
             supplier_car__car=car,
@@ -20,22 +30,42 @@ def best_proposition(cars):
                 .order_by('price'))[0]
         else:
             min_discount_price = None
+        min_discount_price_list.append(min_discount_price)
+
         min_default_price = SupplierCar.objects.filter(
             car=car,
             is_active=True,
         ).order_by('price')[0]
-        if min_discount_price is not None:
-            if min_discount_price.price < min_default_price.price:
-                return {
-                    'supplier': min_discount_price.supplier_car.supplier,
-                    'price': min_discount_price.price,
-                    'car': min_discount_price.supplier_car.car
-                }
-        return {
-            'supplier': min_default_price.supplier,
-            'price': min_default_price.price,
-            'car': min_default_price.car
-        }
+        min_default_price_list.append(min_default_price)
+
+    cheapest_discount_car = get_cheapest_car(min_discount_price_list)
+    cheapest_default_car = get_cheapest_car(min_default_price_list)
+
+    if cheapest_discount_car:
+        if cheapest_discount_car.price < cheapest_default_car.price:
+            return {
+                'supplier': cheapest_discount_car.supplier,
+                'price': cheapest_discount_car.price,
+                'car': cheapest_discount_car.car
+            }
+    return {
+        'supplier': cheapest_default_car.supplier,
+        'price': cheapest_default_car.price,
+        'car': cheapest_default_car.car
+    }
+
+    # if min_discount_price is not None:
+    #     if min_discount_price.price < min_default_price.price:
+    #         return {
+    #             'supplier': min_discount_price.supplier_car.supplier,
+    #             'price': min_discount_price.price,
+    #             'car': min_discount_price.supplier_car.car
+    #         }
+    # return {
+    #     'supplier': min_default_price.supplier,
+    #     'price': min_default_price.price,
+    #     'car': min_default_price.car
+    # }
 
 
 @transaction.atomic
